add56b2be0ba2332de50ba1362d9b8041b11fc4f
rosaspace <rosaspace@126.com>
2024-10-03 15:48:21 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// 如果您想开始测量应用性能，请传递一个函数
+// 来记录结果（例如：reportWebVitals(console.log)）
+// 或发送到分析端点。了解更多：https://bit.ly/CRA-vitals
 reportWebVitals();
